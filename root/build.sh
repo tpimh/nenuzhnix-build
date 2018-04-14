dd69b29777bc918f037ba0115acfc70e72694306
Dmitry Golovin <dima@golovin.in>
2018-04-14 15:02:28 +0300

    add a list of packages to build

--- root/build.sh
+++ root/build.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+export PATH=$PATH:/x86_64-pc-linux-musl/bin:/nenuzhnix-tools
+export LD_LIBRARY_PATH=/x86_64-pc-linux-musl/lib
+
+cd /nenuzhnix
+./download-src.sh
+
+while read PKG; do
+  cd /nenuzhnix/$PKG
+  opkg-buildpackage
+done < /order.txt
+
 echo done
