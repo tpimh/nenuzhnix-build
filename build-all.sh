1a0b10da71c02b1def700bbdc4810da4ed8417a9
Dmitry Golovin <dima@golovin.in>
2018-04-14 12:33:15 +0300

    show the package name, but not the progress bar

--- build-all.sh
+++ build-all.sh
@@ -13,9 +13,9 @@ tar xf apk-tools-static-$APKVER.apk -C root sbin/apk.static 2>/dev/null
 rm apk-tools-static-$APKVER.apk
 cp -r root/* $ROOT
 
-./proot -S $ROOT /sbin/apk.static -q -X $REPO -U --no-cache --allow-untrusted --initdb add alpine-base
+./proot -S $ROOT /sbin/apk.static --no-progress -X $REPO -U --no-cache --allow-untrusted --initdb add alpine-base
 ./proot -S $ROOT /bin/sh -c "echo $REPO > /etc/apk/repositories"
-./proot -S $ROOT /sbin/apk -q --no-cache add libstdc++
-./proot -S $ROOT /sbin/apk -q --no-cache add git curl make findutils tar
+./proot -S $ROOT /sbin/apk --no-progress --no-cache add libstdc++
+./proot -S $ROOT /sbin/apk --no-progress --no-cache add git curl make findutils tar
 ./proot -S $ROOT /usr/bin/env -i /bin/sh -l /install.sh
 ./proot -S $ROOT /usr/bin/env -i /bin/sh -l /build.sh
