ce4185ad6817416f31d2fafaec845516084b79ae
Dmitry Golovin <dima@golovin.in>
2018-04-15 11:05:20 +0300

    install packages after they are built

--- root/build.sh
+++ root/build.sh
@@ -3,6 +3,11 @@
 export PATH=$PATH:/x86_64-pc-linux-musl/bin:/nenuzhnix-tools
 export LD_LIBRARY_PATH=/x86_64-pc-linux-musl/lib
 
+install_opk() {
+  cd /x86_64-pc-linux-musl
+  ar p /nenuzhnix/$1_*.opk data.tar.gz | tar xfz -
+}
+
 SUCCESS=0
 FAIL=0
 
@@ -12,6 +17,7 @@ cd /nenuzhnix
 while read PKG; do
   cd /nenuzhnix/$PKG
   opkg-buildpackage && SUCCESS=$((SUCCESS+1)) || FAIL=$((FAIL+1))
+  install_opk $PKG
 done < /order.txt
 
 echo $SUCCESS packages built, $FAIL packages failed
