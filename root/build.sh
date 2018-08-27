d3f71c04402b604eb181e0645ec87e9bdea1fb78
Dmitry Golovin <dima@golovin.in>
2018-08-27 13:16:47 +0300

    show build output

--- root/build.sh
+++ root/build.sh
@@ -21,7 +21,7 @@ cd /nenuzhnix
 while read PKG; do
   echo -e "travis_fold:start:build\033[33;1mbuilding $PKG\033[0m"
   cd /nenuzhnix/$PKG
-  opkg-buildpackage > /dev/null && SUCCESS=$((SUCCESS+1)) || FAIL=$((FAIL+1))
+  opkg-buildpackage && SUCCESS=$((SUCCESS+1)) || FAIL=$((FAIL+1))
   install_opk $PKG
   echo -e "\ntravis_fold:end:build\r"
 done < /order.txt
