3cf7a875baab72c0cbafc83660aa8f138671c4e8
Dmitry Golovin <dima@golovin.in>
2018-08-27 13:10:41 +0300

    typo fix

--- root/install.sh
+++ root/install.sh
@@ -13,5 +13,5 @@ tar xfz x86_64-pc-linux-musl.tar.gz
 echo -e "\ntravis_fold:end:download\r"
 echo -e "travis_fold:start:download\033[33;1mdownloading sources\033[0m"
 cd /nenuzhnix
-./download-all.sh
+./download-src.sh
 echo -e "\ntravis_fold:end:download\r"
