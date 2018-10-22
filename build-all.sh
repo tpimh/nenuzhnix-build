d252144bf830cd9bab95661932b6e39580fa0bb3
Dmitry Golovin <dima@golovin.in>
2018-10-22 21:13:12 +0300

    make fold variables specific to travis build

--- build-all.sh
+++ build-all.sh
@@ -16,10 +16,10 @@ tar xf apk-tools-static-$APKVER.apk -C root sbin/apk.static 2>/dev/null
 rm apk-tools-static-$APKVER.apk APKINDEX.tar.gz APKINDEX
 cp -r root/* $ROOT
 
-echo "travis_fold:start:download\033[33;1minstalling base system\033[0m"
+printf "%s\033[33;1m%s\033[0m\n" "$FOLD_START" "installing base system"
 ./proot -S $ROOT /sbin/apk.static --no-progress -X $REPO -U --no-cache --allow-untrusted --initdb add alpine-base
 ./proot -S $ROOT /bin/sh -c "echo $REPO > /etc/apk/repositories"
 ./proot -S $ROOT /sbin/apk --no-progress --no-cache add libstdc++ git curl wget make findutils tar coreutils bash automake autoconf libtool cmake ninja file patch bison libarchive-tools
-echo "\ntravis_fold:end:download\r"
+printf "%s\n" "$FOLD_END"
 ./proot -S $ROOT /usr/bin/env -i /bin/sh -l /install.sh
 ./proot -S $ROOT /usr/bin/env -i /bin/sh -l /build.sh
