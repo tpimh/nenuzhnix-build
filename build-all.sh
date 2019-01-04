9df7597b8398eeaee86ad619e6b63993f2f9933c
Dmitry Golovin <dima@golovin.in>
2019-01-04 21:30:17 +0200

    don't download content if it exists locally

--- build-all.sh
+++ build-all.sh
@@ -16,6 +16,16 @@ tar xf apk-tools-static-$APKVER.apk -C root sbin/apk.static 2>/dev/null
 rm apk-tools-static-$APKVER.apk APKINDEX.tar.gz APKINDEX
 cp -r root/* $ROOT
 
+if [ -f x86_64-pc-linux-musl.tar.gz ]
+then
+  cp x86_64-pc-linux-musl.tar.gz $ROOT
+fi
+
+if [ -d nenuzhnix-source ]
+then
+  cp -R nenuzhnix-source $ROOT/nenuzhnix
+fi
+
 printf "%s\033[33;1m%s\033[0m\n" "$FOLD_START" "installing base system"
 ./proot -S $ROOT /sbin/apk.static --no-progress -X $REPO -U --no-cache --allow-untrusted --initdb add alpine-base
 ./proot -S $ROOT /bin/sh -c "echo $REPO > /etc/apk/repositories"
--- root/install.sh
+++ root/install.sh
@@ -5,10 +5,20 @@ NGTC='https://golovin.in/x86_64-pc-linux-musl.tar.gz'
 
 cd /
 echo -e "travis_fold:start:download\033[33;1mcloning nenuzhnix\033[0m"
-git clone $NENREPO
+if [ ! -e nenuzhnix ]
+then
+  git clone $NENREPO
+else
+  echo using local copy of nenuzhnix
+fi
 echo -e "\ntravis_fold:end:download\r"
 echo -e "travis_fold:start:download\033[33;1mobtaining ngtc\033[0m"
-curl --progress-bar -O $NGTC
+if [ ! -e x86_64-pc-linux-musl.tar.gz ]
+then
+  curl --progress-bar -O $NGTC
+else
+  echo using local copy of ngtc
+fi
 tar xfz x86_64-pc-linux-musl.tar.gz 
 echo -e "\ntravis_fold:end:download\r"
 echo -e "travis_fold:start:download\033[33;1mdownloading sources\033[0m"
