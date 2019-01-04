48fdfdae4f23bc60cf7e85a8e5c3820b58afd1f7
Dmitry Golovin <dima@golovin.in>
2019-01-05 01:07:28 +0200

    add some extra space

--- build-all.sh
+++ build-all.sh
@@ -48,7 +48,7 @@ then
   REPO='/repo'
 fi
 
-$(which echo) -e "$FOLD_START\033[33;1minstalling base system\033[0m"
+$(which echo) -e "\n$FOLD_START\033[33;1minstalling base system\033[0m"
 ./proot -S $ROOT /sbin/apk.static --no-progress -X $REPO -U --no-cache --allow-untrusted --initdb add alpine-base
 ./proot -S $ROOT /bin/sh -c "echo $REPO > /etc/apk/repositories"
 ./proot -S $ROOT /sbin/apk --no-progress --no-cache add libstdc++ git curl wget make findutils tar coreutils bash automake autoconf libtool cmake ninja file patch bison libarchive-tools
