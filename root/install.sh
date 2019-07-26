b11306871e7c7aee084cc485f39ffba9ab482e5c
Dmitry Golovin <dima@golovin.in>
2019-07-26 22:00:23 +0300

    in docker ngtc should already be installed

--- root/install.sh
+++ root/install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 NENREPO='https://github.com/tpimh/nenuzhnix.git'
-NGTC='https://golovin.in/x86_64-pc-linux-musl.tar.gz'
 
 cd /
 echo -e "$FOLD_START\033[33;1mcloning nenuzhnix\033[0m"
@@ -11,15 +10,6 @@ then
 else
   echo using local copy of nenuzhnix
 fi
-echo -e "\n$FOLD_END"
-echo -e "$FOLD_START\033[33;1mobtaining ngtc\033[0m"
-if [ ! -e x86_64-pc-linux-musl.tar.gz ]
-then
-  curl --progress-bar -O $NGTC
-else
-  echo using local copy of ngtc
-fi
-tar xfz x86_64-pc-linux-musl.tar.gz 
 echo -e "\n$FOLD_END"
 echo -e "$FOLD_START\033[33;1mdownloading sources\033[0m"
 cd /nenuzhnix
