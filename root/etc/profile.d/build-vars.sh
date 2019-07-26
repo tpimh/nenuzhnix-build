fa904904b694eb30c6ef4ac1fca2839d98604186
Dmitry Golovin <dima@golovin.in>
2019-07-26 21:22:14 +0300

    export variables used during the build

---
+++ root/etc/profile.d/build-vars.sh
@@ -0,0 +1,4 @@
+export PATH=$PATH:/x86_64-pc-linux-musl/bin:/nenuzhnix-tools
+export LD_LIBRARY_PATH=/x86_64-pc-linux-musl/lib
+export PKG_CONFIG_SYSROOT_DIR=/x86_64-pc-linux-musl
+export PKG_CONFIG_LIBDIR=/x86_64-pc-linux-musl/lib/pkgconfig
